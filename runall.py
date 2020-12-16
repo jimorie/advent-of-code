dbc3f4fd3bf3fc4aa14f0984eb0a27965ff55f8a
Petter Nyström <jimorie@gmail.com>
2020-12-16 09:17:21 +0100

    Add year directory

--- runall.py
+++ runall.py
@@ -5,14 +5,17 @@ import time
 
 if __name__ == "__main__":
     rootdir = pathlib.Path(__file__).parent
-    inputdir = rootdir / "input"
-    for inputfile in sorted(inputdir.glob("*")):
-        for pythonfile in sorted(rootdir.glob(f"{inputfile.name}*.py")):
-            with open(inputfile, "r") as fh:
-                print(f"{pythonfile}: ", end="")
-                sys.stdout.flush()
-                sys.stdin = fh
-                execstart = time.time()
-                exec(pythonfile.read_text())
-                exectime = time.time() - execstart
-                print(f"    ({exectime:.3f} s)")
+    for yeardir in sorted(rootdir.glob("2*")):
+        sys.path.append(str(yeardir))
+        inputdir = yeardir / "input"
+        for inputfile in sorted(inputdir.glob("*")):
+            for pythonfile in sorted(yeardir.glob(f"{inputfile.name}*.py")):
+                with open(inputfile, "r") as fh:
+                    print(f"{pythonfile}: ", end="")
+                    sys.stdout.flush()
+                    sys.stdin = fh
+                    execstart = time.time()
+                    exec(pythonfile.read_text())
+                    exectime = time.time() - execstart
+                    print(f"    ({exectime:.3f} s)")
+        sys.path.pop()
